5f17c2c703962dd93f23c741bb432901d1180b0f
BautiCutini <bauticutinii@gmail.com>
2026-04-21 16:53:29 -0300

    Realizo galeria de fotos estatica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+const fulImgBox = document.getElementById("fulImgBox");
+fulImg = document.getElementById("fulImg");
+
+/*funcion para abrir la imagen seleccionada*/
+function openFullImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference;
+}
+
+/*funcion para cerrar img y volver a la galeria*/
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
 
