a7e7c294392c725c9e82d035b266a86fe51994cf
Barteto <bautitelefono@gmail.com>
2026-04-22 16:40:12 -0300

    Agrego los botones que categorizan a los autos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+let categoriaActual = "todas";
 
+const botones = document.querySelectorAll(".btn-categoria");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+
+    categoriaActual = boton.dataset.categoria;
+
+    console.log("Categoría seleccionada:", categoriaActual);
+
+    filtrar(); 
+    });
+});
