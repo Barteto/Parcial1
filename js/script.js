e1fb7996146928e778d52959711e3c406d834175
Barteto <bautitelefono@gmail.com>
2026-04-26 19:53:54 -0300

    Hago que funcionen los botones para filtrar

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
-/*funcion que inicializa el proceso de filtrar*/
 let categoriaActual = "todas";
-/**/
-const fulImgBox = document.getElementById("fulImgBox");
-fulImg = document.getElementById("fulImg");
 
-/*funcion para abrir la imagen seleccionada*/
+const fulImgBox = document.getElementById("fulImgBox");
+const fulImg = document.getElementById("fulImg");
+
 function openFullImg(reference){
     fulImgBox.style.display = "flex";
     fulImg.src = reference;
 }
 
-/*funcion para cerrar img y volver a la galeria*/
 function closeImg(){
     fulImgBox.style.display = "none";
 }
 
-
 const botones = document.querySelectorAll(".btn-categoria");
-/*cuenta boton por boton*/
+
 botones.forEach(boton => {
-    /*cuando hace click el usuario detecta y inicializa el evento*/
     boton.addEventListener("click", () => {
-
-    categoriaActual = boton.dataset.categoria;
-
-    console.log("Categoría seleccionada:", categoriaActual);
-
-    filtrar(); 
+        categoriaActual = boton.dataset.categoria;
+        console.log("Categoría seleccionada:", categoriaActual);
+        filtrar(); 
     });
 });
+
+
+
+function filtrar(){
+    const imagenes = document.querySelectorAll(".img-gallery img");
+
+    imagenes.forEach(img => {
+        const categoriaImg = img.dataset.categoria;
+
+        if(categoriaActual === "todas" || categoriaImg === categoriaActual){
+            img.style.display = "block";
+        } else {
+            img.style.display = "none";
+        }
+    });
+}
