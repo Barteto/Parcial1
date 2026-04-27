ecbee6fc0ceaf0d4491078fc3ff65bc983685cd6
lautarocapdeville1 <lautarocapdeville0@gmail.com>
2026-04-27 15:31:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@
 let categoriaActual = "todas";
 let textoBusqueda = "";
 
-// elementos
+const fulImgBox = document.getElementById("fulImgBox");
+const fulImg = document.getElementById("fulImg");
+
+/*funcion para abrir la imagen seleccionada*/
+function openFullImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference;
+}
+
+/*funcion para cerrar img y volver a la galeria*/
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+// agarra las elementos para q funcione
 const botones = document.querySelectorAll(".btn-categoria");
 const buscador = document.getElementById("buscador");
 
-// botones
+// recorre cada boton y le agrega un evento de click
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         categoriaActual = boton.dataset.categoria;
@@ -14,7 +28,7 @@ botones.forEach(boton => {
     });
 });
 
-// buscador
+// cada vez q el usuario escriba algo se actualiza el textoBusqueda y se llama a filtrar
 buscador.addEventListener("input", () => {
     textoBusqueda = buscador.value.toLowerCase();
     filtrar();
@@ -22,6 +36,7 @@ buscador.addEventListener("input", () => {
 
 // función principal
 function filtrar(){
+    //trae todos los autos y los recorre para mostrar u ocultar segun la categoria y el texto de busqueda
     const cards = document.querySelectorAll(".auto-card");
     let visibles = 0;
 
@@ -39,7 +54,8 @@ function filtrar(){
             card.style.display = "none";
         }
     });
-
+    
+    // actualiza el contador de resultados
     const contador = document.getElementById("contador");
     contador.textContent = `Mostrando ${visibles} resultados`;
 }
