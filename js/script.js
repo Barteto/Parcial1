57ffe02c26864c97dba763a08bc0bece3f686c5f
Santino <santinocrivera13@gmail.com>
2026-04-26 22:27:33 -0300

    js de la barra de busquedas y correcciones en el header

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ function openFullImg(reference){
 function closeImg(){
     fulImgBox.style.display = "none";
 }
+
+
+
+
+const searchBar = document.querySelector(".search-bar"); //retorna el primer elemento que coinciden con el selector
+const cards = document.querySelectorAll(".auto-card"); //retorna todos los elementos que coinciden con el selector
+
+/* funcion para convertir el input a minusculas */ 
+searchBar.addEventListener("input", function() {
+  const texto = this.value.toLowerCase().trim();
+  cards.forEach(filtrarCard);
+  
+
+function filtrarCard(card) { // filtro para mostrar solo las cards que coincidan con el input
+    const nombre = card.querySelector("h3").textContent.toLowerCase();
+    const marca  = card.dataset.categoria;
+
+    if (nombre.includes(texto) || marca.includes(texto)) { //si el nombre o la marca coinciden con el input retorna true
+        card.style.display = "block";
+    } else {
+        card.style.display = "none";
+    }
+}
+});
